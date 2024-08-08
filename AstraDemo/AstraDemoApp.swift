95aa23de79675331294be84a69f5dae760530f60
Rick <rick@agora.io>
2024-08-08 16:50:33 -0700

    Initial Commit

---
+++ AstraDemo/AstraDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstraDemoApp.swift
+//  AstraDemo
+//
+//  Created by Rick Cheng on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AstraDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
